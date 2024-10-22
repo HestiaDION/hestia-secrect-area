0d202c9a2f9f59a4ed2390a6e3b05ec3c2372fca
cruzanab <ana.cruz@germinare.org.br>
2024-10-22 11:31:50 -0300

    Finalização do container de dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './styles/App.css'
 import Header from './components/Header/Header.jsx'
 import Main from './components/Main/Main.jsx';
 import Statistics from './components/Statistics/Statistics.jsx';
+import Dashboard from './components/Dashboard/Dashboard.jsx';
 
 function App() {
 
@@ -11,6 +12,7 @@ function App() {
       <Header/>
       <Main/>
       <Statistics/>
+      <Dashboard/>
     </>
   )
 }
